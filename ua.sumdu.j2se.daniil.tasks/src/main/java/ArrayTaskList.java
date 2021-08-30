6045bdd6a57c854e02e9ee497e6e29e6b397c96c
Lilpank <nikita37pobeditel@mail.ru>
2021-08-30 23:08:47 +0300

    update task-2

--- ua.sumdu.j2se.daniil.tasks/src/main/java/ArrayTaskList.java
+++ ua.sumdu.j2se.daniil.tasks/src/main/java/ArrayTaskList.java
@@ -1,33 +1,58 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 /**
- * Класс содержит в себе list задач и различные методы для list.
+ * Класс содержит в себе массив объектов Task и различные методы для list.
  */
+
 public class ArrayTaskList {
 
-    private final List<Task> taskArrayList = new ArrayList<>();
+    private static final int INITIAL_CAPACITY = 10;
+    private int size = 0;
+    private Task[] elementData = {};
 
 
-    public void add(Task task) {
-        taskArrayList.add(task);
+    public ArrayTaskList() {
+        elementData = new Task[INITIAL_CAPACITY];
     }
 
+    public void add(Task task) {
+        if (size == elementData.length) {
+            ensureCapacity(); // вызывается метод, если закончился массив и нужно создать новый с большим размером.
+        }
+        elementData[size++] = task;
+    }
+
+
+    //Метод создает новый массив с большим размером.
+    private void ensureCapacity() {
+        int newIncreasedCapacity = (elementData.length * 3) / 2 + 1;
+        elementData = Arrays.copyOf(elementData, newIncreasedCapacity);
+    }
+
+
     public boolean remove(Task task) {
-        for (Task element : taskArrayList) {
-            if (element.getTitle().equals(task.getTitle())) {
-                return taskArrayList.remove(element);
+        for (int i = 0; i < size; i++) {
+            if (equalsTasks(elementData[i], task)) {
+                if (size - 1 - i >= 0) System.arraycopy(elementData, i + 1, elementData, i, size - 1 - i);
+                size--;
+                return true;
             }
         }
         return false;
     }
 
+    //Метод проверяет задачи на эквивалентность.
+    private boolean equalsTasks(Task element, Task task) {
+        return element.getTitle().equals(task.getTitle()) && element.getTime() == task.getTime();
+    }
+
     public int size() {
-        return taskArrayList.size();
+        return elementData.length;
     }
 
     public Task getTask(int index) {
-        return taskArrayList.get(index);
+        return elementData[index];
     }
 
     /**
@@ -35,20 +60,28 @@ public class ArrayTaskList {
      *
      * @param from - time
      * @param to   - time
-     * @return list
+     * @return
      */
-    public ArrayList<Task> incoming(int from, final int to) {
-        ArrayList<Task> temp = new ArrayList<>();
-        for (Task element : taskArrayList) {
+    public ArrayList<Task> incoming(int from, int to) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        for (Task element : elementData) {
             if (isIntervalValid(element, from, to)) {
-                temp.add(element);
+                tasks.add(element);
             }
         }
-        return temp;
+        return tasks;
     }
 
-    // Проверка, входит ли задача в заданный интервал [from, to]
+
+    // Метод проверяет, входит ли задача в заданный интервал [from, to]
     private boolean isIntervalValid(Task element, int from, int to) {
         return (from <= element.getStartTime() && element.getStartTime() <= to) || (from <= element.getTime() && element.getTime() <= to);
+    }
+
+    public void display() {
+        System.out.print("Displaying list : ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(elementData[i].getTime() + " ");
+        }
     }
 }
