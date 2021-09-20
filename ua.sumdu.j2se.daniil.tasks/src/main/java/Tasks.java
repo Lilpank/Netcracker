6882df52b9a4067489a0625dcbb32c300ae8917b
Lilpank <nikita37pobeditel@mail.ru>
2021-09-20 20:17:56 +0300

    Не пон 7 задания

--- ua.sumdu.j2se.daniil.tasks/src/main/java/Tasks.java
+++ ua.sumdu.j2se.daniil.tasks/src/main/java/Tasks.java
@@ -1,10 +1,16 @@
 import java.time.LocalDateTime;
-import java.util.Set;
-import java.util.SortedMap;
+import java.util.*;
 
 public class Tasks {
     public static Iterable<Task> incoming(Iterable<Task> tasks, LocalDateTime start, LocalDateTime end) {
-       return null;
+        // Не понял задания..
+        Iterator<Task> iterator = tasks.iterator();
+        while (iterator.hasNext()) {
+            if (!(iterator.next().getStartTime().isAfter(start) || iterator.next().getStartTime().isBefore(end))) {
+                iterator.remove();
+            }
+        }
+        return tasks;
     }
 
     public SortedMap<LocalDateTime, Set<Task>> calendar(Iterable<Task> tasks, LocalDateTime start, LocalDateTime end) {
